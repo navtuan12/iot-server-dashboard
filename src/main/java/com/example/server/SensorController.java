da78bd3cad081dbc374c22ab92eebfdc1efb9608
anhtuan <anhtuan.mmt@gmail.com>
2024-05-24 19:09:28 +0700

    fix sensorservice

--- src/main/java/com/example/server/SensorController.java
+++ src/main/java/com/example/server/SensorController.java
@@ -1,10 +1,19 @@
 package com.example.server;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import com.github.kokorin.jaffree.StreamType;
+import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
+import com.github.kokorin.jaffree.ffmpeg.PipeOutput;
+
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,4 +36,35 @@ public class SensorController {
     public ResponseEntity<String> getGaugeData() {
         return new ResponseEntity<String>(service.getGaugeData(), HttpStatus.OK);
     }
+
+    @GetMapping("/live")
+    @ResponseBody
+    public ResponseEntity<StreamingResponseBody> liveStream(){
+        String rtspUrl = "rtsp://pi:1@100.117.47.33:554";
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(os -> {
+                FFmpeg.atPath()
+                     .addArgument("-re")
+                            .addArguments("-acodec", "pcm_s16le")
+                            .addArguments("-rtsp_transport", "tcp")
+                            .addArguments("-i", rtspUrl)
+                            .addArguments("-vcodec", "copy")
+                            .addArguments("-af", "asetrate=22050")
+                            .addArguments("-acodec", "aac")
+                            .addArguments("-b:a", "96k" )
+                            .addOutput(PipeOutput.pumpTo(os)
+                                    .disableStream(StreamType.AUDIO)
+                                    .disableStream(StreamType.SUBTITLE)
+                                    .disableStream(StreamType.DATA)
+                                    .setFrameCount(StreamType.VIDEO, 100L)
+                                     //1 frame every 10 seconds
+                                    .setFrameRate(0.1)
+                                    .setDuration(1, TimeUnit.HOURS)
+                                    .setFormat("ismv"))
+                            .addArgument("-nostdin")
+                            .execute();
+            });
+    }   
 }
